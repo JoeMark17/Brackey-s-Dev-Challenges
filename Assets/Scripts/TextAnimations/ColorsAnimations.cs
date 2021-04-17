9e5cdd5d2f5f89231f4f3969f6e308f4f7c620ff
Joseph Markiewicz <joseph.markiewicz@gmail.com>
2021-04-17 17:01:10 -0500

    work in progress still

--- Assets/Scripts/TextAnimations/ColorsAnimations.cs
+++ Assets/Scripts/TextAnimations/ColorsAnimations.cs
@@ -7,25 +7,38 @@ using TMPro;
 public class ColorsAnimations : MonoBehaviour
 {
     [SerializeField]
-    TextMeshProUGUI easyText;
-    public string[] easyCharacter;
+    TMP_Text easyText;
+    public string easyCharacter;
 
     private void Awake()
     {
         //easyText.color = new Color32(255, 128, 0, 255);
-        easyCharacter = new string[easyText.text.Length];
+        easyCharacter = easyText.text;
+        StartCoroutine(TypeSentence(easyCharacter));
+
+        //Debug.Log(easyCharacter);
         
-        for (int i = 0; i < easyText.text.Length; i++)
-        {
-            easyText.color = UnityEngine.Random.ColorHSV();
-            easyCharacter[i] = easyText.text.Substring (i, 1);
-            Debug.Log(easyCharacter[i]);
+        
+        // for (int i = 0; i < easyText.text.Length; i++)
+        // {
+        //     easyCharacter[i] = easyText.text.Substring (i, 1);
+        //     Debug.Log(easyCharacter[i]);
 
-            easyText.SetText($"{easyCharacter[i].AddColor(Color.red)}");
+            
 
-           // easyCharacter[i].color = UnityEngine.Random.ColorHSV();
+        //    // easyCharacter[i].color = UnityEngine.Random.ColorHSV();
 
-        }
+        // }
  
     }
+    IEnumerator TypeSentence (string textAnimation)
+    {
+        foreach (char letter in textAnimation)
+        {
+            Debug.Log(letter);
+            yield return null;
+        }
+
+    }
+    
 }
