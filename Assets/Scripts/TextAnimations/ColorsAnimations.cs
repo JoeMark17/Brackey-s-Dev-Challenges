a4f114564fa8e1a146dfdb98d21448665dd3122f
Joseph Markiewicz <joseph.markiewicz@gmail.com>
2021-04-17 13:41:29 -0500

    easy challenge wip

---
+++ Assets/Scripts/TextAnimations/ColorsAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ColorsAnimations : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI easyText;
+    public string[] easyCharacter;
+
+    private void Awake()
+    {
+        //easyText.color = new Color32(255, 128, 0, 255);
+        easyCharacter = new string[easyText.text.Length];
+        
+        for (int i = 0; i < easyText.text.Length; i++)
+        {
+            easyText.color = UnityEngine.Random.ColorHSV();
+            easyCharacter[i] = easyText.text.Substring (i, 1);
+            Debug.Log(easyCharacter[i]);
+
+            easyText.SetText($"{easyCharacter[i].AddColor(Color.red)}");
+
+           // easyCharacter[i].color = UnityEngine.Random.ColorHSV();
+
+        }
+ 
+    }
+}
